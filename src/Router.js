30a70a8ca40cafc922909c5586bf7a5b7b49d2bc
seo0yoon <genious921225@gmail.com>
2022-04-19 23:46:48 +0900

    ADD: MAIN 페이지 UI 구현 완료/리뷰 수정

--- src/Router.js
+++ src/Router.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HostingRouter from './pages/Hosting/HostingRouter';
 import User from './pages/User';
+import Main from './pages/Main';
+import Staylist from './pages/Staylist';
+import Staydetail from './pages/Staydetail';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Main />} />
         <Route path="/user/*" element={<User />} />
         <Route path="/hosting/*" element={<HostingRouter />} />
+        <Route path="/staylist" element={<Staylist />} />
+        <Route path="/staydetail" element={<Staydetail />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Nav/Modal/LocationModal.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LocationModal() {
+  return <div>LocationModal</div>;
+}
+
+export default LocationModal;
